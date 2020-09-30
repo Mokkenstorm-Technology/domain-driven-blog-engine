7de6cae7742292f97c3ec5d4145c058af768c54d
Niels Mokkentstorm <nielsmokkenstorm@gmail.com>
2020-09-30 19:19:38 +0200

    Add disk test for permissive warnings

---
+++ tests/Unit/DiskTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Infrastructure\Support\Disk\{File, FileAccessException};
+
+beforeEach(fn () => $this->old_reporting = error_reporting(E_ALL));
+    
+afterEach(fn () => error_reporting($this->old_reporting));
+
+it('should be able to open files', function() { 
+
+    $path = __DIR__ . '/../../storage/test.txt';
+
+    file_put_contents($path, 'Foo');
+
+    $this->assertEquals('Foo', (new File($path))->content());
+
+    unlink($path);
+});
+
+it('it should fail on unknown files', fn () =>
+
+    (new File('foo'))->content()
+
+)->throws(FileAccessException::class);
+
+it('it should fail on unknown files even with shitty warning config', function () {
+
+    $settings = error_reporting(E_ERROR | E_PARSE);
+
+    (new File('foo'))->content();
+
+})->throws(FileAccessException::class); 
+
